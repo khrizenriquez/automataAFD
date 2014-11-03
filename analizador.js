6dcbba07740319e5a18475154e946f66bd5393e2
khrizenriquez <root@khriz-enriquez.(none)>
2014-11-03 12:41:09 -0600

    Usando JS con use strict y colocando el botón para que se vea genial en vez del input file

--- analizador.js
+++ analizador.js
@@ -1,35 +1,55 @@
 //Randi Guarcas 
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+	subiendoImagen("boton", "btnProceso");
+});
+
 var datos = "";
 var pilaTransiciones = "";
 var pilaEstado = "";
-var tablaTemp = new Array();
-var tablaTrans = new Array();
+var tablaTemp = [];
+var tablaTrans = [];
 var estados = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+//		Haciendo que el input de subir imagen se vea decente
+function subiendoImagen (idFile, idBoton) {
+    var wrapper = $("<div style='height: 0; width: 0; overflow: hidden;'><div>");
+    var fileImagen = $("#" + idFile).wrap(wrapper);
+
+    //todos los evento que tenga se los quito
+    $("#" + idBoton).off("click");
+
+    //agrego el evento click para que lo escuche el elemento
+    $("#" + idBoton).on("click", function(event, objetct) {
+        fileImagen.click();
+    }).show();
+}
 
 function loadAutomata(f) {
 	var reader = new FileReader();  
 	reader.readAsText(f);      
 	reader.onload = function() { 
 
-		file_text = reader.result;
+		var file_text = reader.result;
 		datos = file_text.split("\n"); 
 		
 		pilaTransiciones = getTransiciones(datos[4].toString());
 		pilaEstado = getEstados(datos[0].toString());
-		pilaAlfabeto = getEstados(datos[1].toString());
-		g = 1;
-		pilaMueve = [pilaEstado[0]];
-		T = transicionLambda(pilaMueve);
+		var pilaAlfabeto = getEstados(datos[1].toString());
+		var g = 1;
+		var pilaMueve = [pilaEstado[0]];
+		var T = transicionLambda(pilaMueve);
 		
 		tablaTemp = [null,estados[0],null,null,T];
 		tablaTrans = [tablaTemp];
 
-		for(mi=0;mi<tablaTrans.length;mi++){
+		for(var mi=0;mi<tablaTrans.length;mi++){
 			while(tablaTrans[mi][0] == null){
 				tablaTrans[mi][0] = 0;//marcado
-				for(x of pilaAlfabeto){
+				for(var x in pilaAlfabeto) {
 					if(g<15){
-						U = transicionLambda(mueveEstado(tablaTrans[mi][1],x));
+						var U = transicionLambda(mueveEstado(tablaTrans[mi][1],x));
 							if(!appendTablaTrans(U,tablaTrans)){
 								tablaTemp = [null,estados[g],null,null,U];
 								tablaTrans.push(tablaTemp)
@@ -59,7 +79,7 @@ function loadAutomata(f) {
 		
 		//alfabeto
 		var alfabeto = "{";
-		for(alf=0;alf<pilaAlfabeto.length;alf++){
+		for(var alf=0;alf<pilaAlfabeto.length;alf++){
 			alfabeto += pilaAlfabeto[alf] + "  "
 		}
 		alfabeto +=" }"
@@ -78,14 +98,14 @@ function loadAutomata(f) {
 }
 
 function getEstadosDeAceptacion(aceptacion){
-cadenaLimpia = quitarPuntoEstadoLlave(aceptacion);
-estadosAceptacion = new Array();
-estados = "{  ";
-tokensAceptacion = cadenaLimpia.split(",");
-	for(acepta of tokensAceptacion){
-		for(y=0;y<tablaTrans.length;y++){
-			comp = tablaTrans[y][4];
-			for(r=0;r<comp.length;r++){
+	var cadenaLimpia = quitarPuntoEstadoLlave(aceptacion);
+	var estadosAceptacion = new Array();
+	var estados = "{  ";
+	var tokensAceptacion = cadenaLimpia.split(",");
+	for(var acepta in tokensAceptacion){
+		for(var y=0;y<tablaTrans.length;y++){
+			var comp = tablaTrans[y][4];
+			for(var r=0;r<comp.length;r++){
 				if(acepta == comp[r]){
 					estadosAceptacion.push(tablaTrans[y][1]);
 				}
@@ -94,9 +114,9 @@ tokensAceptacion = cadenaLimpia.split(",");
 			//
 		}
 	}
-	estadosTrans = estadosAceptacion.unique().sort();
+	var estadosTrans = estadosAceptacion.unique().sort();
 	
-	for(y=0;y<estadosTrans.length;y++){
+	for(var y=0;y<estadosTrans.length;y++){
 		estados += estadosTrans[y]+"  ";
 	}
 	estados += "}"
@@ -104,56 +124,56 @@ tokensAceptacion = cadenaLimpia.split(",");
 }
 
 function getEstadosTablaTransiciones(){
-estados = "{ ";
-	for(y=0;y<tablaTrans.length;y++){
+	var estados = "{ ";
+	for(var y=0;y<tablaTrans.length;y++){
 		estados += tablaTrans[y][1] + "  ";
 	}
-estados += "}";
-return estados;
+	estados += "}";
+	return estados;
 }
  
 function getEstadoComposicion(composicion,tabla){
-encontrado = false;
-	for(y=0;y<tabla.length;y++){
+	var encontrado = false;
+	for(var y=0;y<tabla.length;y++){
 		comp = tabla[y][4];
 		if(comp==composicion){
 			encontrado = tabla[y][1];
 		}
 	}
-return encontrado;
+	return encontrado;
 }
 function getHTMLtabla(tablaTrans){
-tablaHTML = "<table class='tablaTransD'>";
-tablaHTML += "<thead class='head'><tr><th>ESTADO</th><th>a</th><th>b</th><th>COMPOSICIÓN</th></tr></thead>";
-for (i=0;i<tablaTrans.length;i++){
-	tablaHTML += "<tr>";
-    for (j=1;j<tablaTrans[i].length;j++){
-	
-	tablaHTML +="<td class='cols'>" + tablaTrans[i][j] + "</td>";
-    }
-tablaHTML += "</tr>";
-}
-tablaHTML += "</table>";
+	var tablaHTML = "<table class='tablaTransD'>";
+	tablaHTML += "<thead class='head'><tr><th>ESTADO</th><th>a</th><th>b</th><th>COMPOSICIÓN</th></tr></thead>";
+	for (var i=0;i<tablaTrans.length;i++){
+		tablaHTML += "<tr>";
+	    for (var j=1;j<tablaTrans[i].length;j++){
+		
+		tablaHTML +="<td class='cols'>" + tablaTrans[i][j] + "</td>";
+	    }
+	tablaHTML += "</tr>";
+	}
+	tablaHTML += "</table>";
 
-return tablaHTML;
+	return tablaHTML;
 }
 
 function appendTablaTrans(composicion, tabla){
-encontrado = false;
-	for(y=0;y<tabla.length;y++){
-		comp = tabla[y][4];
+	var encontrado = false;
+	for(var y=0;y<tabla.length;y++){
+		var comp = tabla[y][4];
 		if(comp==composicion){
 			encontrado = true;
 		}
 	}
-return encontrado;
+	return encontrado;
 }
 
 function buscarTrans(composicion){
-nuevo = 0;
-contador = 1;
+	var nuevo = 0;
+	var contador = 1;
 	console.log(composicion);
-	for(y=0;y<tablaTrans.length;y++){
+	for(var y=0;y<tablaTrans.length;y++){
 		if(tablaTrans[y][4]==composicion){
 			console.log("encontrado en:" + tablaTrans[y][1]);
 		}else{
@@ -161,27 +181,27 @@ contador = 1;
 		}
 		
 	}
-return nuevo;
+	return nuevo;
 }
 
 function mueveEstado(estado,elementoAlfabeto){
-var apilar = new Array();
-var pilaMueve = new Array();
+	var apilar = [];
+	var pilaMueve = [];
 	//console.log("mueve("+estado+","+elementoAlfabeto+")");
 	
-	for(n=0;n<tablaTrans.length;n++){
-		estadoTabla = tablaTrans[n][1];
+	for(var n=0;n<tablaTrans.length;n++){
+		var estadoTabla = tablaTrans[n][1];
 		if(estadoTabla == estado){
-			compocisionTabla = tablaTrans[n][4];
-			for(elemento of compocisionTabla){
+			var compocisionTabla = tablaTrans[n][4];
+			for(var elemento in compocisionTabla){
 				apilar.push(elemento);
 			}	
 		}
 	}
 	
-	for(mueve of apilar){
-		for (transicion of pilaTransiciones) {
-		transicionUnique = transicion;
+	for(var mueve in apilar) {
+		for (var transicion in pilaTransiciones) {
+		var transicionUnique = transicion;
 			if(transicionUnique[1] == elementoAlfabeto){
 				if(transicionUnique[0]==mueve){
 					pilaMueve.push(transicionUnique[2]);
@@ -199,14 +219,14 @@ var pilaMueve = new Array();
 }
 
 function transicionLambda(pilaMueve){
-estadosLambda = "";
-pilaLambda=new Array();
-retornarPila = 0;
+	var estadosLambda = "";
+	var pilaLambda = [];
+	var retornarPila = 0;
 
-	for(mueve of pilaMueve){
+	for(var mueve in pilaMueve) {
 		if(mueve!="0"){
-			for (transicion of pilaTransiciones) {
-				transicionUnique = transicion;
+			for (var transicion in pilaTransiciones) {
+				var transicionUnique = transicion;
 				if(transicionUnique[0]==mueve){
 					if(transicionUnique[1]=="e"){
 						pilaLambda.push(transicionUnique[2]);
@@ -218,8 +238,8 @@ retornarPila = 0;
 	}
 
 	if(pilaLambda.length > 0){
-		for(mueve of pilaLambda){
-			for (transicion of pilaTransiciones) {
+		for(var mueve in pilaLambda){
+			for (var transicion in pilaTransiciones) {
 			transicionUnique = transicion;
 				if(transicionUnique[0]==mueve){
 					if(transicionUnique[1]=="e"){
@@ -228,7 +248,7 @@ retornarPila = 0;
 				}	
 			}
 		}
-		for(m of pilaMueve){
+		for(var m in pilaMueve){
 			pilaLambda.push(m);
 		}		
 	
@@ -236,9 +256,9 @@ retornarPila = 0;
 		pilaLambda = pilaMueve;	
 	}
 	
-	pilaLimpia = pilaLambda.unique().sort();
-	for(char of pilaLimpia){
-		estadosLambda +=char;
+	var pilaLimpia = pilaLambda.unique().sort();
+	for(var ch in pilaLimpia) {
+		estadosLambda += ch;
 	}
 	//console.log("{"+estadosLambda+"}");
 	return estadosLambda;	
@@ -249,10 +269,10 @@ Array.prototype.unique=function(a){
 });
 
 function getAlfabeto(estadosCadena){
-	cadenaLimpia = quitarPuntoEstadoLlave(estadosCadena);
-	estadosTokens = cadenaLimpia.split(",");
-	pilaAlfabeto = new Array();
-	for(i=0;i<estadosTokens.length;i++){
+	var cadenaLimpia = quitarPuntoEstadoLlave(estadosCadena);
+	var estadosTokens = cadenaLimpia.split(",");
+	var pilaAlfabeto = [];
+	for(var i=0;i<estadosTokens.length;i++){
 		pilaAlbafeto.push(estadosTokens[i]);
 	}
 	return pilaAlfabeto;
@@ -260,10 +280,10 @@ function getAlfabeto(estadosCadena){
 
 
 function getEstados(estadosCadena){
-	cadenaLimpia = quitarPuntoEstadoLlave(estadosCadena);
-	estadosTokens = cadenaLimpia.split(",");
-	pilaEstados = new Array();
-	for(i=0;i<estadosTokens.length;i++){
+	var cadenaLimpia = quitarPuntoEstadoLlave(estadosCadena);
+	var estadosTokens = cadenaLimpia.split(",");
+	var pilaEstados = [];
+	for(var i=0;i<estadosTokens.length;i++){
 		pilaEstados.push(estadosTokens[i]);
 	}
 	return pilaEstados;
@@ -271,24 +291,24 @@ function getEstados(estadosCadena){
 
 
 function getTransiciones(estadosCadena){
-	cadenaLimpia = quitarPuntoEstadoLlave(estadosCadena);
-	apilar ="";
-	pilaTransiciones = new Array();	
-	for(i = 0; i < cadenaLimpia.length; i++){
-		char = cadenaLimpia[i];
-		if(char!=','){
-			if(char=='('){
-				char = char.replace("(", "|");
+	var cadenaLimpia = quitarPuntoEstadoLlave(estadosCadena);
+	var apilar ="";
+	var pilaTransiciones = [];	
+	for(var i = 0; i < cadenaLimpia.length; i++){
+		var ch = cadenaLimpia[i];
+		if(ch!=','){
+			if(ch=='('){
+				ch = ch.replace("(", "|");
 			}
-			if(char==')'){
-				char = char.replace(")", "|");
+			if(ch==')'){
+				ch = ch.replace(")", "|");
 			}
-			apilar+=char;
+			apilar+=ch;
 		}
 	}
 	
-	trans =	apilar.split("|");
-	for(i=0;i<trans.length;i++){
+	var trans =	apilar.split("|");
+	for(var i=0;i<trans.length;i++){
 		if(trans[i]!=''){
 			pilaTransiciones.push(trans[i]);
 		}
@@ -298,11 +318,11 @@ function getTransiciones(estadosCadena){
 }
 
 function quitarPuntoEstadoLlave(cadena){
-	cadenaLimpia = "";
-		post = cadena.split(":");
-		pre = post[1].split("{");
-		ini = pre[1].split("}");
-	for(i = 0; i < ini.length; i++){
+	var cadenaLimpia = "";
+	var post = cadena.split(":");
+	var pre = post[1].split("{");
+	var ini = pre[1].split("}");
+	for(var i = 0; i < ini.length; i++){
 		cadenaLimpia += ini[i];
 	}	
 	return cadenaLimpia;
